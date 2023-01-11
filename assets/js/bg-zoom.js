034b4ade4d9ff6ab679ba61271195a70941e6c7a
Luxray997 <develo120@gmail.com>
2023-01-10 22:21:26 -0600

    update files
    Add deck generator and changed moving background on main page

--- assets/js/bg-zoom.js
+++ assets/js/bg-zoom.js
@@ -2,9 +2,9 @@ const bg = document.querySelector('.welcome-background');
 const windowWidth = window.innerWidth / 5;
 const windowHeight = window.innerHeight / 5 ;
 
-document.addEventListener('mousemove', (e) => {
-  const mouseX = e.clientX / windowWidth;
-  const mouseY = e.clientY / windowHeight;
+// document.addEventListener('mousemove', (e) => {
+//   const mouseX = e.clientX / windowWidth;
+//   const mouseY = e.clientY / windowHeight;
   
-  bg.style.transform = `translate3d(-${mouseX}%, -${mouseY}%, 0)`;
-});
+//   bg.style.transform = `translate3d(-${mouseX}%, -${mouseY}%, 0)`;
+// });
---
+++ assets/js/deck-generator.js
@@ -0,0 +1,799 @@
+let cardList = [
+    {
+       "name":"Knight",
+       "elixir":3,
+       "type":"Troop",
+       "rarity":"Common",
+       "id":26000000
+    },
+    {
+       "name":"Archers",
+       "elixir":3,
+       "type":"Troop",
+       "rarity":"Common",
+       "id":26000001
+    },
+    {
+       "name":"Goblins",
+       "elixir":2,
+       "type":"Troop",
+       "rarity":"Common",
+       "id":26000002
+    },
+    {
+       "name":"Giant",
+       "elixir":5,
+       "type":"Troop",
+       "rarity":"Rare",
+       "id":26000003
+    },
+    {
+       "name":"P.E.K.K.A",
+       "elixir":7,
+       "type":"Troop",
+       "rarity":"Epic",
+       "id":26000004
+    },
+    {
+       "name":"Minions",
+       "elixir":3,
+       "type":"Troop",
+       "rarity":"Common",
+       "id":26000005
+    },
+    {
+       "name":"Balloon",
+       "elixir":5,
+       "type":"Troop",
+       "rarity":"Epic",
+       "id":26000006
+    },
+    {
+       "name":"Witch",
+       "elixir":5,
+       "type":"Troop",
+       "rarity":"Epic",
+       "id":26000007
+    },
+    {
+       "name":"Barbarians",
+       "elixir":5,
+       "type":"Troop",
+       "rarity":"Common",
+       "id":26000008
+    },
+    {
+       "name":"Golem",
+       "elixir":8,
+       "type":"Troop",
+       "rarity":"Epic",
+       "id":26000009
+    },
+    {
+       "name":"Skeletons",
+       "elixir":1,
+       "type":"Troop",
+       "rarity":"Common",
+       "id":26000010
+    },
+    {
+       "name":"Valkyrie",
+       "elixir":4,
+       "type":"Troop",
+       "rarity":"Rare",
+       "id":26000011
+    },
+    {
+       "name":"Skeleton Army",
+       "elixir":3,
+       "type":"Troop",
+       "rarity":"Epic",
+       "id":26000012
+    },
+    {
+       "name":"Bomber",
+       "elixir":2,
+       "type":"Troop",
+       "rarity":"Common",
+       "id":26000013
+    },
+    {
+       "name":"Musketeer",
+       "elixir":4,
+       "type":"Troop",
+       "rarity":"Rare",
+       "id":26000014
+    },
+    {
+       "name":"Baby Dragon",
+       "elixir":4,
+       "type":"Troop",
+       "rarity":"Epic",
+       "id":26000015
+    },
+    {
+       "name":"Prince",
+       "elixir":5,
+       "type":"Troop",
+       "rarity":"Epic",
+       "id":26000016
+    },
+    {
+       "name":"Wizard",
+       "elixir":5,
+       "type":"Troop",
+       "rarity":"Rare",
+       "id":26000017
+    },
+    {
+       "name":"Mini P.E.K.K.A",
+       "elixir":4,
+       "type":"Troop",
+       "rarity":"Rare",
+       "id":26000018
+    },
+    {
+       "name":"Spear Goblins",
+       "elixir":2,
+       "type":"Troop",
+       "rarity":"Common",
+       "id":26000019
+    },
+    {
+       "name":"Giant Skeleton",
+       "elixir":6,
+       "type":"Troop",
+       "rarity":"Epic",
+       "id":26000020
+    },
+    {
+       "name":"Hog Rider",
+       "elixir":4,
+       "type":"Troop",
+       "rarity":"Rare",
+       "id":26000021
+    },
+    {
+       "name":"Minion Horde",
+       "elixir":5,
+       "type":"Troop",
+       "rarity":"Common",
+       "id":26000022
+    },
+    {
+       "name":"Ice Wizard",
+       "elixir":3,
+       "type":"Troop",
+       "rarity":"Legendary",
+       "id":26000023
+    },
+    {
+       "name":"Royal Giant",
+       "elixir":6,
+       "type":"Troop",
+       "rarity":"Common",
+       "id":26000024
+    },
+    {
+       "name":"Guards",
+       "elixir":3,
+       "type":"Troop",
+       "rarity":"Epic",
+       "id":26000025
+    },
+    {
+       "name":"Princess",
+       "elixir":3,
+       "type":"Troop",
+       "rarity":"Legendary",
+       "id":26000026
+    },
+    {
+       "name":"Dark Prince",
+       "elixir":4,
+       "type":"Troop",
+       "rarity":"Epic",
+       "id":26000027
+    },
+    {
+       "name":"Three Musketeers",
+       "elixir":9,
+       "type":"Troop",
+       "rarity":"Rare",
+       "id":26000028
+    },
+    {
+       "name":"Lava Hound",
+       "elixir":7,
+       "type":"Troop",
+       "rarity":"Legendary",
+       "id":26000029
+    },
+    {
+       "name":"Ice Spirit",
+       "elixir":1,
+       "type":"Troop",
+       "rarity":"Common",
+       "id":26000030
+    },
+    {
+       "name":"Fire Spirit",
+       "elixir":1,
+       "type":"Troop",
+       "rarity":"Common",
+       "id":26000031
+    },
+    {
+       "name":"Miner",
+       "elixir":3,
+       "type":"Troop",
+       "rarity":"Legendary",
+       "id":26000032
+    },
+    {
+       "name":"Sparky",
+       "elixir":6,
+       "type":"Troop",
+       "rarity":"Legendary",
+       "id":26000033
+    },
+    {
+       "name":"Bowler",
+       "elixir":5,
+       "type":"Troop",
+       "rarity":"Epic",
+       "id":26000034
+    },
+    {
+       "name":"Lumberjack",
+       "elixir":4,
+       "type":"Troop",
+       "rarity":"Legendary",
+       "id":26000035
+    },
+    {
+       "name":"Battle Ram",
+       "elixir":4,
+       "type":"Troop",
+       "rarity":"Rare",
+       "id":26000036
+    },
+    {
+       "name":"Inferno Dragon",
+       "elixir":4,
+       "type":"Troop",
+       "rarity":"Legendary",
+       "id":26000037
+    },
+    {
+       "name":"Ice Golem",
+       "elixir":2,
+       "type":"Troop",
+       "rarity":"Rare",
+       "id":26000038
+    },
+    {
+       "name":"Mega Minion",
+       "elixir":3,
+       "type":"Troop",
+       "rarity":"Rare",
+       "id":26000039
+    },
+    {
+       "name":"Dart Goblin",
+       "elixir":3,
+       "type":"Troop",
+       "rarity":"Rare",
+       "id":26000040
+    },
+    {
+       "name":"Goblin Gang",
+       "elixir":3,
+       "type":"Troop",
+       "rarity":"Common",
+       "id":26000041
+    },
+    {
+       "name":"Electro Wizard",
+       "elixir":4,
+       "type":"Troop",
+       "rarity":"Legendary",
+       "id":26000042
+    },
+    {
+       "name":"Elite Barbarians",
+       "elixir":6,
+       "type":"Troop",
+       "rarity":"Common",
+       "id":26000043
+    },
+    {
+       "name":"Hunter",
+       "elixir":4,
+       "type":"Troop",
+       "rarity":"Epic",
+       "id":26000044
+    },
+    {
+       "name":"Executioner",
+       "elixir":5,
+       "type":"Troop",
+       "rarity":"Epic",
+       "id":26000045
+    },
+    {
+       "name":"Bandit",
+       "elixir":3,
+       "type":"Troop",
+       "rarity":"Legendary",
+       "id":26000046
+    },
+    {
+       "name":"Royal Recruits",
+       "elixir":7,
+       "type":"Troop",
+       "rarity":"Common",
+       "id":26000047
+    },
+    {
+       "name":"Night Witch",
+       "elixir":4,
+       "type":"Troop",
+       "rarity":"Legendary",
+       "id":26000048
+    },
+    {
+       "name":"Bats",
+       "elixir":2,
+       "type":"Troop",
+       "rarity":"Common",
+       "id":26000049
+    },
+    {
+       "name":"Royal Ghost",
+       "elixir":3,
+       "type":"Troop",
+       "rarity":"Legendary",
+       "id":26000050
+    },
+    {
+       "name":"Ram Rider",
+       "elixir":5,
+       "type":"Troop",
+       "rarity":"Legendary",
+       "id":26000051
+    },
+    {
+       "name":"Zappies",
+       "elixir":4,
+       "type":"Troop",
+       "rarity":"Rare",
+       "id":26000052
+    },
+    {
+       "name":"Rascals",
+       "elixir":5,
+       "type":"Troop",
+       "rarity":"Common",
+       "id":26000053
+    },
+    {
+       "name":"Cannon Cart",
+       "elixir":5,
+       "type":"Troop",
+       "rarity":"Epic",
+       "id":26000054
+    },
+    {
+       "name":"Mega Knight",
+       "elixir":7,
+       "type":"Troop",
+       "rarity":"Legendary",
+       "id":26000055
+    },
+    {
+       "name":"Skeleton Barrel",
+       "elixir":3,
+       "type":"Troop",
+       "rarity":"Common",
+       "id":26000056
+    },
+    {
+       "name":"Flying Machine",
+       "elixir":4,
+       "type":"Troop",
+       "rarity":"Rare",
+       "id":26000057
+    },
+    {
+       "name":"Wall Breakers",
+       "elixir":2,
+       "type":"Troop",
+       "rarity":"Epic",
+       "id":26000058
+    },
+    {
+       "name":"Royal Hogs",
+       "elixir":5,
+       "type":"Troop",
+       "rarity":"Rare",
+       "id":26000059
+    },
+    {
+       "name":"Goblin Giant",
+       "elixir":6,
+       "type":"Troop",
+       "rarity":"Epic",
+       "id":26000060
+    },
+    {
+       "name":"Fisherman",
+       "elixir":3,
+       "type":"Troop",
+       "rarity":"Legendary",
+       "id":26000061
+    },
+    {
+       "name":"Magic Archer",
+       "elixir":4,
+       "type":"Troop",
+       "rarity":"Legendary",
+       "id":26000062
+    },
+    {
+       "name":"Electro Dragon",
+       "elixir":5,
+       "type":"Troop",
+       "rarity":"Epic",
+       "id":26000063
+    },
+    {
+       "name":"Firecracker",
+       "elixir":3,
+       "type":"Troop",
+       "rarity":"Common",
+       "id":26000064
+    },
+    {
+       "name":"Elixir Golem",
+       "elixir":3,
+       "type":"Troop",
+       "rarity":"Rare",
+       "id":26000067
+    },
+    {
+       "name":"Battle Healer",
+       "elixir":4,
+       "type":"Troop",
+       "rarity":"Rare",
+       "id":26000068
+    },
+    {
+       "name":"Skeleton Dragons",
+       "elixir":4,
+       "type":"Troop",
+       "rarity":"Common",
+       "id":26000080
+    },
+    {
+       "name":"Mother Witch",
+       "elixir":4,
+       "type":"Troop",
+       "rarity":"Legendary",
+       "id":26000083
+    },
+    {
+       "name":"Electro Spirit",
+       "elixir":1,
+       "type":"Troop",
+       "rarity":"Common",
+       "id":26000084
+    },
+    {
+       "name":"Electro Giant",
+       "elixir":7,
+       "type":"Troop",
+       "rarity":"Epic",
+       "id":26000085
+    },
+    {
+       "name":"Phoenix",
+       "elixir":4,
+       "type":"Troop",
+       "rarity":"Legendary",
+       "id":26000087
+    },
+    {
+       "name":"Cannon",
+       "elixir":3,
+       "type":"Building",
+       "rarity":"Common",
+       "id":27000000
+    },
+    {
+       "name":"Goblin Hut",
+       "elixir":5,
+       "type":"Building",
+       "rarity":"Rare",
+       "id":27000001
+    },
+    {
+       "name":"Mortar",
+       "elixir":4,
+       "type":"Building",
+       "rarity":"Common",
+       "id":27000002
+    },
+    {
+       "name":"Inferno Tower",
+       "elixir":5,
+       "type":"Building",
+       "rarity":"Rare",
+       "id":27000003
+    },
+    {
+       "name":"Bomb Tower",
+       "elixir":4,
+       "type":"Building",
+       "rarity":"Rare",
+       "id":27000004
+    },
+    {
+       "name":"Barbarian Hut",
+       "elixir":6,
+       "type":"Building",
+       "rarity":"Rare",
+       "id":27000005
+    },
+    {
+       "name":"Tesla",
+       "elixir":4,
+       "type":"Building",
+       "rarity":"Common",
+       "id":27000006
+    },
+    {
+       "name":"Elixir Collector",
+       "elixir":6,
+       "type":"Building",
+       "rarity":"Rare",
+       "id":27000007
+    },
+    {
+       "name":"X-Bow",
+       "elixir":6,
+       "type":"Building",
+       "rarity":"Epic",
+       "id":27000008
+    },
+    {
+       "name":"Tombstone",
+       "elixir":3,
+       "type":"Building",
+       "rarity":"Rare",
+       "id":27000009
+    },
+    {
+       "name":"Furnace",
+       "elixir":4,
+       "type":"Building",
+       "rarity":"Rare",
+       "id":27000010
+    },
+    {
+       "name":"Goblin Cage",
+       "elixir":4,
+       "type":"Building",
+       "rarity":"Rare",
+       "id":27000012
+    },
+    {
+       "name":"Goblin Drill",
+       "elixir":4,
+       "type":"Building",
+       "rarity":"Epic",
+       "id":27000013
+    },
+    {
+       "name":"Fireball",
+       "elixir":4,
+       "type":"Spell",
+       "rarity":"Rare",
+       "id":28000000
+    },
+    {
+       "name":"Arrows",
+       "elixir":3,
+       "type":"Spell",
+       "rarity":"Common",
+       "id":28000001
+    },
+    {
+       "name":"Rage",
+       "elixir":2,
+       "type":"Spell",
+       "rarity":"Epic",
+       "id":28000002
+    },
+    {
+       "name":"Rocket",
+       "elixir":6,
+       "type":"Spell",
+       "rarity":"Rare",
+       "id":28000003
+    },
+    {
+       "name":"Goblin Barrel",
+       "elixir":3,
+       "type":"Spell",
+       "rarity":"Epic",
+       "id":28000004
+    },
+    {
+       "name":"Freeze",
+       "elixir":4,
+       "type":"Spell",
+       "rarity":"Epic",
+       "id":28000005
+    },
+    {
+       "name":"Mirror",
+       "elixir":1,
+       "type":"Spell",
+       "rarity":"Epic",
+       "id":28000006
+    },
+    {
+       "name":"Lightning",
+       "elixir":6,
+       "type":"Spell",
+       "rarity":"Epic",
+       "id":28000007
+    },
+    {
+       "name":"Zap",
+       "elixir":2,
+       "type":"Spell",
+       "rarity":"Common",
+       "id":28000008
+    },
+    {
+       "name":"Poison",
+       "elixir":4,
+       "type":"Spell",
+       "rarity":"Epic",
+       "id":28000009
+    },
+    {
+       "name":"Graveyard",
+       "elixir":5,
+       "type":"Spell",
+       "rarity":"Legendary",
+       "id":28000010
+    },
+    {
+       "name":"The Log",
+       "elixir":2,
+       "type":"Spell",
+       "rarity":"Legendary",
+       "id":28000011
+    },
+    {
+       "name":"Tornado",
+       "elixir":3,
+       "type":"Spell",
+       "rarity":"Epic",
+       "id":28000012
+    },
+    {
+       "name":"Clone",
+       "elixir":3,
+       "type":"Spell",
+       "rarity":"Epic",
+       "id":28000013
+    },
+    {
+       "name":"Earthquake",
+       "elixir":3,
+       "type":"Spell",
+       "rarity":"Rare",
+       "id":28000014
+    },
+    {
+       "name":"Barbarian Barrel",
+       "elixir":2,
+       "type":"Spell",
+       "rarity":"Epic",
+       "id":28000015
+    },
+    {
+       "name":"Heal Spirit",
+       "elixir":1,
+       "type":"Troop",
+       "rarity":"Rare",
+       "id":28000016
+    },
+    {
+       "name":"Giant Snowball",
+       "elixir":2,
+       "type":"Spell",
+       "rarity":"Common",
+       "id":28000017
+    },
+    {
+       "name":"Royal Delivery",
+       "elixir":3,
+       "type":"Spell",
+       "rarity":"Common",
+       "id":28000018
+    },
+    {
+       "name":"Archer Queen",
+       "elixir":5,
+       "type":"Troop",
+       "rarity":"Champion",
+       "id":26000072
+    },
+    {
+       "name":"Mighty Miner",
+       "elixir":4,
+       "type":"Troop",
+       "rarity":"Champion",
+       "id":26000065
+    },
+    {
+       "name":"Golden Knight",
+       "elixir":4,
+       "type":"Troop",
+       "rarity":"Champion",
+       "id":26000074
+    },
+    {
+       "name":"Skeleton King",
+       "elixir":4,
+       "type":"Troop",
+       "rarity":"Champion",
+       "id":26000069
+    },
+    {
+       "name":"Monk",
+       "elixir":5,
+       "type":"Troop",
+       "rarity":"Champion",
+       "id":26000077
+    }
+]
+
+
+function createRandomDeckLink() {
+    let deck = [];
+    let link = "https://link.clashroyale.com/deck/en?deck=";
+    let hasChampion = false;
+    for (i = 0; i < 8; i++) {
+        let card = cardList[randomInt(0, cardList.length)];
+        if (!deck.includes(card) && (!hasChampion || card.rarity != "Champion")) {
+            deck.push(card);
+            link += card.id + ";";
+        } else {
+            i--;
+        }
+    }
+
+    return link + "&id=2Q8C888LL"
+    
+}
+
+function randomInt(start, rangeSize) {
+    return start + Math.floor(Math.random() * rangeSize);
+}
+
+function copyLinkToClipboard() {
+    navigator.clipboard.writeText(createRandomDeckLink());
+}
+
+function linkRandomDeck() {
+    let link = createRandomDeckLink();
+    let element = document.getElementById("link");
+    element.innerHTML = "Open Link";
+    element.setAttribute("href", link);
+}
