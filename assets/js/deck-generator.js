1e4cac0782c9e7b75b2c1986bca9f432368680a4
Luxray997 <develo120@gmail.com>
2023-01-23 22:35:13 -0600

    update files
    - finished deck generator logic and functionality

--- assets/js/deck-generator.js
+++ assets/js/deck-generator.js
@@ -5,33 +5,52 @@ import  {
    randomInt 
 }  from './util.js';
 
+window.rarityMap = new Map();
+rarityMap.set("Common", 0);
+rarityMap.set("Rare", 1);
+rarityMap.set("Epic", 2);
+rarityMap.set("Legendary", 3);
+rarityMap.set("Champion", 4);
+
 window.mode = "hide";
-window.excludeArr = [];
-window.includeArr = [];
-window.cardArr = [];
+window.excludeArr = new Map();
+window.includeArr = new Map();
+window.includeHasChampion = false;
+window.cardArr = new Map();
 populateFilterCardList()
 
 document.getElementById("filter-form").addEventListener("change", filterOptionClicked);
 document.getElementsByClassName("generate-button")[0].addEventListener("click", linkRandomDeck);
+document.getElementById("filter-hide-button").addEventListener("click", hideFilterClicked);
 
-function filterOptionClicked() {
+function filterOptionClicked(hide) {
    mode = getValueFromRadioButtons(document.getElementsByClassName("filter-option"));
    let cardContainer = document.getElementById("card-filter-container");
    let includeUl = document.getElementById("include-filter-ul");
    let excludeUl = document.getElementById("exclude-filter-ul");
+   let noneUl = document.getElementById("none-filter-ul");
    includeUl.classList.remove("selected-filter");
    excludeUl.classList.remove("selected-filter");
-   if (mode == "hide") {
+   noneUl.classList.remove("grayed-out-container");
+   noneUl.classList.remove("grayed-out-champion-container");
+   if (hide == "hide") {
       cardContainer.style.display = "none";
+      document.getElementById("filter-form").reset();
    } else if ( mode=="include" ) {
       includeUl.classList.add("selected-filter");
-      cardContainer.style.display = "block";
+      if (includeArr.size >= 8)
+         noneUl.classList.add("grayed-out-container");
+      if (includeHasChampion)
+         noneUl.classList.add("grayed-out-champion-container");
+      cardContainer.style.display = "flex";
    } else if ( mode=="exclude" ) {
       excludeUl.classList.add("selected-filter");
-      cardContainer.style.display = "block";
+      cardContainer.style.display = "flex";
    }
+}
 
-
+function hideFilterClicked() {
+   filterOptionClicked("hide");
 }
 
 function populateFilterCardList() {
@@ -41,29 +60,86 @@ function populateFilterCardList() {
       const li = document.createElement("li");
       const image = document.createElement("img");
       image.setAttribute("src", card.icon);
-      // image.setAttribute("class", card.rarity == "Champion" ? "card-img champion" : "card-img");
+      if (card.rarity == "Champion")
+         image.setAttribute("class", "champion");
       image.setAttribute("alt", card.name);
       image.addEventListener("click", cardClicked);
       li.appendChild(image);
+      for (let entry in card) {
+         image.dataset[entry] = card[entry];
+      }
       cardContainer.appendChild(li);
-      cardArr.push({li, card});
+      cardArr.set(card.name, li);
+   }
+   bubbleSortCardList(cardContainer, compareRarity);
+}
+
+
+
+/*
+   Bubble sort
+*/
+function bubbleSortCardList(cardListUl, compareFunction) {
+   let switchedOnPass = true;
+   let passes = 0;
+   while (switchedOnPass) {
+      switchedOnPass = false;
+      for (let i = 0; i < cardListUl.children.length - 1 - passes; i++) {
+         if (compareFunction(cardListUl.children[i].firstChild, cardListUl.children[i + 1].firstChild)) {
+            cardListUl.insertBefore(cardListUl.children[i + 1], cardListUl.children[i]);
+            switchedOnPass = true;
+         }
+      }
    }
 }
 
+function compareRarity(a, b) {
+   return rarityMap.get(a.dataset.rarity) < rarityMap.get(b.dataset.rarity);
+}
+
 function cardClicked() {
-   let cardPair = cardArr.find(e => e.li == this.parentNode);
-   if (mode == "include") {
-      document.getElementById("include-filter-ul").appendChild(this.parentNode);
-      if (includeArr.length == 6)
-         return;
-      else if (includeArr.length >= 8)
-         return;
-   } else if (mode == "exclude") {
-      document.getElementById("exclude-filter-ul").appendChild(this.parentNode);
-      if (excludeArr.length == 6)
-         return;
-      else if (excludeArr.length >= 8)
-         return;
+   if (this.parentNode.parentNode == document.getElementById("none-filter-ul")) {
+      if (mode == "include") {
+         if (includeHasChampion && this.classList.contains("champion"))
+            return;
+         if (includeArr.size <= 7) {
+            cardArr.delete(this.dataset.name);
+            includeArr.set(this.dataset.name, this.parentNode);
+            document.getElementById("include-filter-ul").appendChild(this.parentNode);
+            if (includeArr.size == 8)
+               document.getElementById("none-filter-ul").classList.add("grayed-out-container");
+            if (this.classList.contains("champion"))
+            {
+               document.getElementById("none-filter-ul").classList.add("grayed-out-champion-container");
+               includeHasChampion = true;
+            }
+         } else
+            return;
+      } else if (mode == "exclude") { 
+         if (cardArr.size <= 8)
+            return;
+         if (cardArr.size == 9) {
+            document.getElementById("none-filter-ul").classList.add("grayed-out-container");
+         }
+         cardArr.delete(this.dataset.name);
+         excludeArr.set(this.dataset.name, this.parentNode);
+         document.getElementById("exclude-filter-ul").appendChild(this.parentNode);
+      }
+   } else if (this.parentNode.parentNode == document.getElementById("include-filter-ul")) {
+      document.getElementById("none-filter-ul").appendChild(this.parentNode);
+      document.getElementById("none-filter-ul").classList.remove("grayed-out-container")
+      includeArr.delete(this.dataset.name);
+      cardArr.set(this.dataset.name, this.parentNode);
+      if (this.classList.contains("champion"))
+         document.getElementById("none-filter-ul").classList.remove("grayed-out-champion-container");
+         includeHasChampion = false;
+      bubbleSortCardList(document.getElementById("none-filter-ul"), compareRarity);
+   } else /* exclude */ {
+      document.getElementById("none-filter-ul").appendChild(this.parentNode);
+      document.getElementById("none-filter-ul").classList.remove("grayed-out-container")
+      excludeArr.delete(this.dataset.name);
+      cardArr.set(this.dataset.name, this.parentNode);
+      bubbleSortCardList(document.getElementById("none-filter-ul"), compareRarity);
    }
 }
 
@@ -73,11 +149,13 @@ function createRandomDeck(excludeArr, includeArr, champion) {
 
    includeArr.forEach(e => {
       deck.push(e);
-      if (e.rarity == champion) 
+      if (e.rarity == "Champion") 
          hasChampion = true;
    });
-   if (champion == 2) {
-      deck.push(championList[randomInt(0, championList.length)]);
+   if (!hasChampion && champion == 2) {
+      let excludedChampionList = championList.filter(e => !excludeArr.some(e2 => e2.id == e.id));
+      if (excludedChampionList.length > 0)
+         deck.push(excludedChampionList[randomInt(0, excludedChampionList.length)]);
       hasChampion = true
    } else if (champion == 0) {
       hasChampion = true;
@@ -85,7 +163,7 @@ function createRandomDeck(excludeArr, includeArr, champion) {
 
    while (deck.length < 8) {
       let card = cardList[randomInt(0, cardList.length)];
-      if (!deck.includes(card) && !excludeArr.includes(card)) {
+      if (!deck.includes(card) && !excludeArr.some(e => e.id == card.id)) {
          if (card.rarity == "Champion") {
             if (hasChampion) continue;
             hasChampion = true;
@@ -112,8 +190,32 @@ function linkRandomDeck() {
       cardContainer.removeChild(cardContainer.firstChild);
    }
 
-   const EXCLUDE_LIST = [];
-   const INCLUDE_LIST = [];
+   let EXCLUDE_LIST = [];
+   for (let li of window.excludeArr.values()) {
+      EXCLUDE_LIST.push(
+         {
+            "name" : li.firstChild.dataset.name,
+            "elixir" : parseInt(li.firstChild.dataset.elixir),
+            "type" : li.firstChild.dataset.type,
+            "rarity" : li.firstChild.dataset.rarity,
+            "id" : parseInt(li.firstChild.dataset.id),
+            "icon" : li.firstChild.dataset.icon,
+         }
+      );
+   }
+   let INCLUDE_LIST = [];
+   for (let li of window.includeArr.values()) {
+      INCLUDE_LIST.push( 
+         {
+            "name" : li.firstChild.dataset.name,
+            "elixir" : parseInt(li.firstChild.dataset.elixir),
+            "type" : li.firstChild.dataset.type,
+            "rarity" : li.firstChild.dataset.rarity,
+            "id" : parseInt(li.firstChild.dataset.id),
+            "icon" : li.firstChild.dataset.icon
+         }
+      );
+   }
    const CHAMPION_SETTING = getValueFromRadioButtons(document.getElementsByClassName("champion-option"));
    let deck = createRandomDeck(EXCLUDE_LIST, INCLUDE_LIST, CHAMPION_SETTING);
    let link = getLinkFromDeck(deck);
